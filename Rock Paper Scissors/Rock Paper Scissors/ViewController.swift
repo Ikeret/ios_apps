246f12dc6cfc3367d4a68b5f596068dab3e6be99
Sergey <79185660148@ya.ru>
2019-07-06 21:56:46 +0300

    init

---
+++ Rock Paper Scissors/Rock Paper Scissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Сергей Коршунов on 06/07/2019.
+//  Copyright © 2019 Sergey Korshunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

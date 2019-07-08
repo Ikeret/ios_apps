27cfc988a9d71a00987c6e7419afba01be0562e5
Sergey <79185660148@ya.ru>
2019-07-08 20:10:56 +0300

    init

---
+++ Numbers/Numbers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Numbers
+//
+//  Created by Сергей Коршунов on 08/07/2019.
+//  Copyright © 2019 Sergey Korshunov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Numbers/Numbers/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Numbers
+//
+//  Created by Сергей Коршунов on 08/07/2019.
+//  Copyright © 2019 Sergey Korshunov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

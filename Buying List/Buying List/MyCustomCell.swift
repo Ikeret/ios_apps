19699b7e4e0ebe4ea0871c79d5fe0b7a1d927448
Sergey <79185660148@ya.ru>
2019-07-10 19:33:25 +0300

    add custom tableViewCells

---
+++ Buying List/Buying List/MyCustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomCell.swift
+//  Buying List
+//
+//  Created by Сергей Коршунов on 10/07/2019.
+//  Copyright © 2019 Sergey Korshunov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCustomCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var count: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+}
--- Buying List/Buying List/ViewController.swift
+++ Buying List/Buying List/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{ //UITableViewDelegate, UITableViewDataSource {
+    struct data_cell {
+        var name : String?
+        var count : UInt?
+        var price : Double?
+    }
+    
+    var data_arr : [data_cell] = [data_cell.init(name: "Молоко", count: 2, price: 35.90)]
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    let identifier = "MyCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data_arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:MyCustomCell = self.myTableView.dequeueReusableCell(withIdentifier: identifier) as! MyCustomCell
+
+        let index = indexPath.row
+        cell.name.text = data_arr[index].name
+        cell.count.text = String(data_arr[index].count!)
+        cell.price.text = String(data_arr[index].price!)
+        
+        return cell;
+    }
+    
+    
+}

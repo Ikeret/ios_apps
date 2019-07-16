26014366cf06d5dc25a08bd995c3f1ddd00ed7d4
Sergey <79185660148@ya.ru>
2019-07-16 03:46:58 +0300

    added saving data

--- Buying List/Buying List/ViewController.swift
+++ Buying List/Buying List/ViewController.swift
@@ -9,31 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-    struct data_cell {
+    struct data_cell : Codable {
         var name : String?
         var count : UInt?
         var price : Double?
     }
     
 
-    var data_arr : [data_cell] = []
-//    {
-//        set {
-//
-//
-//            UserDefaults.standard.set(NSArray(object: newValue), forKey: "dataKey")
-//            UserDefaults.standard.synchronize()
-////            set_sum()
-////            myTableView.reloadData()
-//        }
-//        get {
-//            if let array = UserDefaults.standard.array(forKey: "dataKey") as NSArray? {
-//                return array as! [ViewController.data_cell]
-//            } else {
-//                return []
-//            }
-//        }
-//    }
+    var data_arr : [data_cell] //= []
+    {
+        set {
+            UserDefaults.standard.set(try? PropertyListEncoder().encode(newValue), forKey: "dataKey")
+            UserDefaults.standard.synchronize()
+            set_sum()
+            myTableView.reloadData()
+        }
+        get {
+            if let data = UserDefaults.standard.value(forKey: "dataKey") as? Data  {
+                let array = (try? PropertyListDecoder().decode(Array<data_cell>.self, from: data))!
+                return array
+            } else {
+                return []
+            }
+        }
+    }
 
     @IBOutlet weak var myTableView: UITableView!
     
@@ -43,7 +42,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+       set_sum()
     }
     
     func set_sum() {
@@ -58,22 +58,24 @@ class ViewController: UIViewController {
         let alertController = UIAlertController(title: "Add new item", message: nil, preferredStyle: .alert)
         
         alertController.addTextField { (textField) in
-            textField.placeholder = "Name"
+            textField.placeholder = "Name (required)"
+            textField.autocapitalizationType = .words
         }
         
         alertController.addTextField { (textField) in
-            textField.placeholder = "Count"
+            textField.placeholder = "Price (optional)"
         }
         
         alertController.addTextField { (textField) in
-            textField.placeholder = "Price"
+            textField.placeholder = "Count (optional)"
         }
         
-        let alertAction1 = UIAlertAction(title: "Cancel", style: .default, handler: nil)
-        let alertAction2 = UIAlertAction(title: "Add", style: .cancel) { (alert) in
-            if let name = alertController.textFields![0].text,
-                let count : UInt = UInt(alertController.textFields![1].text!),
-                let price : Double = Double(alertController.textFields![2].text!) {
+        
+        let alertAction1 = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let alertAction2 = UIAlertAction(title: "Add", style: .default) { (alert) in
+            if let name = alertController.textFields![0].text {
+                let count = UInt(alertController.textFields![2].text!) ?? 1
+                let price = Double(alertController.textFields![1].text!) ?? 0.0
                 self.data_arr.append(data_cell(name: name, count: count, price: price))
                 self.myTableView.reloadData()
                 self.set_sum()
@@ -106,14 +108,23 @@ extension ViewController : UITableViewDataSource, UITableViewDelegate {
         let cell:MyCustomCell = self.myTableView.dequeueReusableCell(withIdentifier: identifier) as! MyCustomCell
 
         let index = indexPath.row
-        cell.name.text = data_arr[index].name
-        if data_arr[index].count! != 1 {
-            cell.count.text = "\(data_arr[index].count!)"
-        }
-        else {
+        let name = data_arr[index].name
+        let count = data_arr[index].count!
+        let price = data_arr[index].price!
+        
+        cell.name.text = name
+        
+        if count != 1 {
+            cell.count.text = "\(count)"
+        } else {
             cell.count.text = ""
         }
-        cell.price.text = String(format: "%6.2f", data_arr[index].price!)
+        
+        if price != 0.0 {
+            cell.price.text = String(format: "%6.2f", price)
+        } else {
+            cell.price.text = ""
+        }
         
         return cell;
     }

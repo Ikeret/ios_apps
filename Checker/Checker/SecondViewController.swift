6340233c6bb0374b66cf53f1add653797f704cad
Sergey <79185660148@ya.ru>
2019-07-17 23:30:31 +0300

    init new app

---
+++ Checker/Checker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Checker
+//
+//  Created by Сергей Коршунов on 17/07/2019.
+//  Copyright © 2019 Sergey Korshunov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
